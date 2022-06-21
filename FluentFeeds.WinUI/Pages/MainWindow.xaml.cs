e680c1477a9c9e5c3f8bda0e1309554865572890
Hannes Schulze <haschu0103@gmail.com>
2022-06-21 19:39:04 +0200

    Add a window icon

--- FluentFeeds.WinUI/Pages/MainWindow.xaml.cs
+++ FluentFeeds.WinUI/Pages/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using FluentFeeds.Shared.ViewModels;
+﻿using System.IO;
+using FluentFeeds.Shared.ViewModels;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using Windows.ApplicationModel;
 
 namespace FluentFeeds.WinUI.Pages;
 
@@ -9,6 +13,12 @@ public sealed partial class MainWindow : Window
 	{
 		ViewModel = viewModel;
 		InitializeComponent();
+
+		var windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(this);
+		var windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+		var appWindow = AppWindow.GetFromWindowId(windowId);
+		var iconPath = Path.Combine(Package.Current.InstalledLocation.Path, "Assets", "WindowIcon.ico");
+		appWindow.SetIcon(iconPath);
 	}
 
 	public MainViewModel ViewModel { get; }
