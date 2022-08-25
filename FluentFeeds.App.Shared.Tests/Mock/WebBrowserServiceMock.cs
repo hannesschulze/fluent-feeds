bbc7a3fd665e39d711b7dd4b8c3544fd241750fa
Hannes Schulze <haschu0103@gmail.com>
2022-08-25 20:22:05 +0200

    Update tests

---
+++ FluentFeeds.App.Shared.Tests/Mock/WebBrowserServiceMock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentFeeds.App.Shared.Services;
+
+namespace FluentFeeds.App.Shared.Tests.Mock;
+
+public sealed class WebBrowserServiceMock : IWebBrowserService
+{
+	public sealed class OpenEventArgs : System.EventArgs
+	{
+		public OpenEventArgs(Uri url)
+		{
+			Url = url;
+		}
+		
+		public Uri Url { get; }
+	}
+
+	public event EventHandler<OpenEventArgs>? OpenEvent; 
+
+	public void Open(Uri url)
+	{
+		OpenEvent?.Invoke(this, new OpenEventArgs(url));
+	}
+}
---
+++ FluentFeeds.App.Shared.Tests/ViewModels/Pages/SettingsViewModelTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentFeeds.App.Shared.Models;
+using FluentFeeds.App.Shared.Tests.Mock;
+using FluentFeeds.App.Shared.ViewModels.Pages;
+using Xunit;
+
+namespace FluentFeeds.App.Shared.Tests.ViewModels.Pages;
+
+public class SettingsViewModelTests
+{
+	private SettingsServiceMock SettingsService { get; } = new();
+
+	private WebBrowserServiceMock WebBrowserService { get; } = new();
+	
+	[Fact]
+	public void UpdateAppTheme()
+	{
+		var viewModel = new SettingsViewModel(SettingsService, WebBrowserService);
+		Assert.Equal(Theme.SystemDefault, viewModel.SelectedTheme);
+		SettingsService.AppTheme = Theme.Light;
+		Assert.Equal(Theme.Light, viewModel.SelectedTheme);
+		viewModel.SelectedTheme = Theme.Dark;
+		Assert.Equal(Theme.Dark, SettingsService.AppTheme);
+	}
+
+	[Fact]
+	public void Commands_OpenProjectWebsite()
+	{
+		var viewModel = new SettingsViewModel(SettingsService, WebBrowserService);
+		var openArgs = Assert.Raises<WebBrowserServiceMock.OpenEventArgs>(
+			h => WebBrowserService.OpenEvent += h, h => WebBrowserService.OpenEvent -= h,
+			() => viewModel.OpenProjectWebsiteCommand.Execute(null)).Arguments;
+		Assert.Equal(new Uri(Constants.AppWebsiteUrl), openArgs.Url);
+	}
+
+	[Fact]
+	public void Commands_OpenGitHubIssues()
+	{
+		var viewModel = new SettingsViewModel(SettingsService, WebBrowserService);
+		var openArgs = Assert.Raises<WebBrowserServiceMock.OpenEventArgs>(
+			h => WebBrowserService.OpenEvent += h, h => WebBrowserService.OpenEvent -= h,
+			() => viewModel.OpenGitHubIssuesCommand.Execute(null)).Arguments;
+		Assert.Equal(new Uri(Constants.AppIssuesUrl), openArgs.Url);
+	}
+}
