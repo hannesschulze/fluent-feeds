fabeaa9c4de78f0c827f793d291d791bed2cbb57
Hannes Schulze <haschu0103@gmail.com>
2022-08-28 13:16:19 +0200

    Update spacing

--- website/components/footer.js
+++ website/components/footer.js
@@ -4,23 +4,21 @@ import styles from './footer.module.css';
 
 export default function Footer() {
   return (
-    <footer>
-      <div className={commonStyles.section}>
-        <div className={`${commonStyles.container} ${styles.footer}`}>
-          <span className={styles.copyright}>
-            &copy; 2022, the Fluent Feeds developers
-          </span>
-          <nav className={styles.nav}>
-            <ul>
-              <li>
-                <Link href='/imprint'>Imprint</Link>
-              </li>
-              <li>
-                <Link href='/privacy-policy'>Privacy Policy</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+    <footer className={`${commonStyles.section} ${styles.footer}`}>
+      <div className={`${commonStyles.container} ${styles.footerContainer}`}>
+        <span className={styles.copyright}>
+          &copy; 2022, the Fluent Feeds developers
+        </span>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href='/imprint'>Imprint</Link>
+            </li>
+            <li>
+              <Link href='/privacy-policy'>Privacy Policy</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </footer>
   );
